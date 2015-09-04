2de6c12679c0999053a63a215f136be7cb4fe206
arx50036 <arx50036@AS010007.gruppoarca.it>
2015-09-04 14:44:23 +0200

    aggiungo il progetto creato

---
+++ ProgettoDiProva/src/main/java/it/noccio/progetto/App.java
@@ -0,0 +1,13 @@
+package it.noccio.progetto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
